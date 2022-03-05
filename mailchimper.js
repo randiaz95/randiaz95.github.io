4305c82146c949db679e5ece2f1a53e1b4257a74
randiaz95 <randiaz95@gmail.com>
2022-03-04 19:48:45 -0500

    Add files via upload

---
+++ mailchimper.js
@@ -0,0 +1,1 @@
+(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[5]='MMERGE5';ftypes[5]='text';fnames[0]='EMAIL';ftypes[0]='email';fnames[4]='PHONE';ftypes[4]='phone';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='ADDRESS';ftypes[3]='address';}(jQuery));var $mcj = jQuery.noConflict(true);
